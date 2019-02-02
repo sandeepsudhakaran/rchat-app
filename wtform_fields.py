10525dfc5a95ad0b086793d42f14f8d310c2ea87
Sandeep Sudhakaran <sandeepsudhakaran1@gmail.com>
2019-02-02 14:02:47 -0500

    Move custom validtor inline

--- wtform_fields.py
+++ wtform_fields.py
@@ -1,18 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
 
 from passlib.hash import pbkdf2_sha256
 from models import User
-
-
-def duplicate_username(form, field):
-    """ Duplicate username checker """
-
-    username = field.data
-    user_data = User.query.filter_by(username=username).first()
-    if user_data:
-        raise ValidationError("Username already exists. Select a different username.")
 
 
 def invalid_credentials(form, field):
@@ -34,10 +25,14 @@ def invalid_credentials(form, field):
 class RegistrationForm(FlaskForm):
     """ Registration form"""
 
-    username = StringField('username', validators=[InputRequired(message="Username required"), Length(min=4, max=25, message="Username must be between 4 and 25 characters"), duplicate_username])
+    username = StringField('username', validators=[InputRequired(message="Username required"), Length(min=4, max=25, message="Username must be between 4 and 25 characters")])
     password = PasswordField('password', validators=[InputRequired(message="Password required"), Length(min=4, max=25, message="Password must be between 4 and 25 characters")])
     confirm_pswd = PasswordField('confirm_pswd', validators=[InputRequired(message="Password required"), EqualTo('password', message="Passwords must match")])
 
+    def validate_username(self, username):
+        user_object = User.query.filter_by(username=username.data).first()
+        if user_object:
+            raise ValidationError("Username already exists. Select a different username.")
 
 class LoginForm(FlaskForm):
     """ Login form """
