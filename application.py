f8482ce29c826fa636f6e34dfb86ace73a8ef838
Sandeep Sudhakaran <sandeepsudhakaran1@gmail.com>
2019-02-02 14:13:30 -0500

    Remove duplicate route for 404

--- application.py
+++ application.py
@@ -94,12 +94,6 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    # note that we set the 404 status explicitly
-    return render_template('404.html'), 404
-
-
 @socketio.on('incoming-msg')
 def on_message(data):
     """Broadcast messages"""
